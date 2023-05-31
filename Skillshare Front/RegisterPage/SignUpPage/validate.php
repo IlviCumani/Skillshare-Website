e7e1f5680e3b068cbcbe24bc285f76343a1988cd
ErliK1 <ekuka21@epoka.edu.al>
2023-05-31 12:23:37 +0200

    Sign up validation

--- Skillshare Front/RegisterPage/SignUpPage/validate.php
+++ Skillshare Front/RegisterPage/SignUpPage/validate.php
@@ -6,17 +6,30 @@ $msg = '';
 if (!empty($_POST)) {
     // Post data not empty insert a new record
     // Set-up the variables that are going to be inserted, we must check if the POST variables exist if not we can default them to blank
-    $id = 2;//Kujdes!!!
+    //$id = 2;//Kujdes!!!
+    //$id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id']:NULL;
     // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
     $name = isset($_POST['username']) ? validate($_POST['username']) : '';
     $password = isset($_POST['password']) ? validate($_POST['password']): '';
     $phone = "355699145020";
-    $type = "Learner";//"isset($_POST['type']) ? $_POST['type']: ''";
-    $email = isset($_POST['email']) ? validate($_POST['email']): '';
+    $type = isset($_POST['type'])? $_POST['type']: '';//"isset($_POST['type']) ? $_POST['type']: ''";
+    $email = isset($_POST['email']) && !empty($_POST['email']) ? validate($_POST['email']): '';
     // Insert new record into the contacts table
-    $stmt = $pdo->prepare('INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)');
-    $stmt->execute([$id, $name, $password, $type, $phone, $email]);
+    $the_sql = 'SELECT email FROM users WHERE email = :email';
+    $msg = $pdo->prepare($the_sql);
+    $msg->bindParam(':email',$email,PDO::PARAM_STR);
+    $msg->execute();
+    //$result = $msg->fetchAll(PDO::FETCH_OBJ);
+    if($msg->rowCount()>0){
+
+        echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+        header("Location: signup.php");
+        exit();
+    }
+    $stmt = $pdo->prepare('INSERT INTO users VALUES (?,?, ?, ?, ?, ?)');
+    $stmt->execute([NULL,$name, $password, $type, $phone, $email]);
     // Output message
     $msg = 'Created Successfully!';
+    header("Location: ../../UserPages/ProfilePage/profile.php");
 }
 ?>

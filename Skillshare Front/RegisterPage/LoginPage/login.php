5a1dffd621bb5e66a63ca79b82fa242e0afd77e5
Mateogjini14 <97111201+Mateogjini14@users.noreply.github.com>
2023-06-05 12:51:14 +0200

    changes

--- Skillshare Front/RegisterPage/LoginPage/login.php
+++ Skillshare Front/RegisterPage/LoginPage/login.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../../MainPage/Css/scroll.css">
     <link rel="icon" href="../../MainPage/MainImg/favicon-removebg-preview.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Skillshare360</title>

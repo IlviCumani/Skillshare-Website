2ebf280f94486f1d580a8adb2d8beecffe0eebf4
JoanGjergo <jgjergo21@epoka.edu.al>
2023-06-06 19:28:50 +0200

    update api

---
+++ Skillshare Front/UserPages/ProfilePage/update-profile.php
@@ -0,0 +1,28 @@
+<?php
+// update-profile.php
+session_start();
+require 'thedatab.php';
+require 'profile.php';
+
+$pdo = pdo_connect_mysql();
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Retrieve the submitted form data
+    $name = $_GET['edit-name'];
+    $email = $_GET['edit-email'];
+    $phone = $_GET['edit-phone'];
+    $userType = $_GET['user-type'];
+
+    $userId = $_SESSION['user_id'];
+    $query = "UPDATE users SET name = '$name', email = '$email', phone = '$phone', user_type = '$userType' WHERE id = $userId";
+    $statement = $pdo->prepare($query);
+    $statement->execute();
+
+    
+
+    // Output a response
+    echo "Profile updated successfully!";
+} else {
+    // Invalid request method
+    echo "Invalid request method!";
+}
+?>
